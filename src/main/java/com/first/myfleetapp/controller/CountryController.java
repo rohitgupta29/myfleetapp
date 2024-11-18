11246ef782daa601762be61296fdcd45704a5f23
rohitgupta <rohit.gupta@cognologix.com>
2024-11-18 16:47:48 +0530

    updated heml files and controller files

--- src/main/java/com/first/myfleetapp/controller/CountryController.java
+++ src/main/java/com/first/myfleetapp/controller/CountryController.java
@@ -1,14 +1,25 @@
 package com.first.myfleetapp.controller;
 
+import com.first.myfleetapp.models.Country;
+import com.first.myfleetapp.services.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 
 @Controller
 public class CountryController {
 
+    @Autowired
+    private CountryService countryService;
+
     @GetMapping("/countries")
-    public String getCountries() {
+    public String getCountries(Model model) {
+
+        List<Country> countryList = countryService.getCountries();
+        model.addAttribute("countries", countryList);
         return "country";
     }
 }
--- src/main/java/com/first/myfleetapp/services/CountryService.java
+++ src/main/java/com/first/myfleetapp/services/CountryService.java
@@ -1,8 +1,23 @@
 package com.first.myfleetapp.services;
 
+import com.first.myfleetapp.repositories.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.first.myfleetapp.models.Country;
+
+import java.util.List;
 
 @Service
 public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    //Return list of countries
+    public List<Country> getCountries() {
+        return countryRepository.findAll();
+    }
+
 
 }
